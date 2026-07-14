77d06f6d4847142ee023331949f46c982118ab93
Betül USLU <betulzisanuslu@hotmail.com>
2026-07-14 07:42:08 +0300

    Initial project setup

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Vampire Village</h1>
+      <button>Oyun Oluştur</button>
+      <button>Odaya Katıl</button>
+    </div>
+  );
+}
